af8563a770ce5cfb7eb8fa3dd2ff210c3eb17857
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2022-04-17 18:05:33 -0700

    add support for rotations in scenes

--- src/scene.rs
+++ src/scene.rs
@@ -1,13 +1,14 @@
 use bevy::asset::{Handle, LoadContext, LoadedAsset};
 use bevy::hierarchy::{BuildWorldChildren, WorldChildBuilder};
-use bevy::math::{UVec3};
+use bevy::math::{Mat3, Quat, UVec3, Vec3, Vec4, Vec4Swizzles};
 use bevy::pbr::{PbrBundle, StandardMaterial};
 use bevy::prelude::{Mesh, Transform, World};
 use bevy::scene::Scene;
 use bevy::transform::TransformBundle;
-use dot_vox::{Model, SceneNode};
+use dot_vox::{Dict, Model, SceneNode};
 
 // constants used in magicavoxel's scene graph dictionaries
+const ROTATION: &str = "_r";
 const TRANSLATION: &str = "_t";
 
 pub(crate) fn load_scene(
@@ -43,24 +44,12 @@ fn traverse_scene(
     match root {
         SceneNode::Transform { frames, child, .. } => {
             if let Some(child_root) = scene.get(*child as usize) {
-                let transform = frames
-                    .get(0)
-                    .and_then(|x| x.get(TRANSLATION))
-                    .and_then(|translation| {
-                        let mut components = translation.split(" ");
-                        let x = components.next()?.parse::<f32>().ok()?;
-                        let y = components.next()?.parse::<f32>().ok()?;
-                        let z = components.next()?.parse::<f32>().ok()?;
-                        if components.next() == None {
-                            // note: we swizzle z and y since bevy is y-up
-                            Some(Transform::from_xyz(x, z, y))
-                        } else {
-                            // there shouldn't be more than 3 components, bail
-                            None
-                        }
-                    })
-                    .map(|x| root_transform * x)
-                    .unwrap_or(root_transform);
+                let this_transform = Transform {
+                    translation: extract_translation(frames).unwrap_or_default(),
+                    rotation: extract_rotation(frames).unwrap_or_default(),
+                    ..Transform::default()
+                };
+                let transform = root_transform * this_transform;
 
                 traverse_scene(builder, scene, child_root, transform, models, material, meshes);
             }
@@ -76,18 +65,68 @@ fn traverse_scene(
             for model in shape_models {
                 let id = model.model_id as usize;
                 if let (Some(mesh), Some(model)) = (meshes.get(id), models.get(id)) {
-                    // note: we swizzle z and y since bevy is y-up
-                    let pivot = UVec3::new(model.size.x / 2, model.size.z / 2, model.size.y / 2).as_vec3();
-                    let mut transform = root_transform.clone();
-                    transform.translation -= pivot;
+                    // we swizzle z and y since bevy is y-up
+                    let size = UVec3::new(model.size.x, model.size.z, model.size.y).as_vec3();
+                    // `load_from_model` adds a 1-voxel border around the entire model, which we
+                    // need to account for when calculating the pivot
+                    let pivot = dbg!((size / 2.0).floor() + 1.0);
+                    let translation =  dbg!(root_transform.mul_vec3(-pivot).floor());
                     builder.spawn_bundle(PbrBundle {
                         mesh: mesh.clone(),
                         material: material.clone(),
-                        transform,
+                        transform: Transform {
+                            translation,
+                            ..root_transform
+                        },
                         ..PbrBundle::default()
                     });
                 }
             }
         }
     }
+}
+
+fn extract_translation(frame: &Vec<Dict>) -> Option<Vec3> {
+    frame
+        .get(0)
+        .and_then(|x| x.get(TRANSLATION))
+        .and_then(|translation| {
+            let mut components = translation.split(" ");
+            let x = components.next()?.parse::<f32>().ok()?;
+            let y = components.next()?.parse::<f32>().ok()?;
+            let z = components.next()?.parse::<f32>().ok()?;
+            if components.next() == None {
+                // we swizzle z and y since bevy is y-up
+                Some(Vec3::new(x, z, y))
+            } else {
+                // there shouldn't be more than 3 components, bail
+                None
+            }
+        })
+}
+
+// Based on https://github.com/jpaver/opengametools/blob/master/src/ogt_vox.h#L821
+fn extract_rotation(frame: &Vec<Dict>) -> Option<Quat> {
+    frame
+        .get(0)
+        .and_then(|x| x.get(ROTATION))
+        .and_then(|translation| {
+            let packed = translation.parse::<u32>().ok()?;
+            let index0 = packed & 0b11;
+            let index1 = (packed >> 2u32) & 0b11;
+            let index2 = (1u32 << index0 | 1u32 << index1).trailing_ones();
+
+            #[inline(always)]
+            fn negate_if(x: u32) -> f32 {
+                if x == 0 { 1.0 } else { -1.0 }
+            }
+
+            let mut mat = Mat3::ZERO;
+            mat.x_axis[index0 as usize] = negate_if(packed & (1 << 4));
+            mat.y_axis[index1 as usize] = negate_if(packed & (1 << 5));
+            mat.z_axis[index2 as usize] = negate_if(packed & (1 << 6));
+
+            // we swizzle z and y since bevy is y-up
+            Some(Quat::from_vec4(Vec4::from(Quat::from_mat3(&mat)).xzyw()))
+        })
 }
